5b571cb108616daa0156b6b152d40d6d88a75d8a
roggrt <42749056+roggrt@users.noreply.github.com>
2024-10-28 09:22:45 -0500

    inclusion ahora si definitiva de google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,38 @@
+// // src/app/layout.tsx
+// import { ThemeProvider } from '@/components/providers/ThemeProvider'
+// import './globals.css'
+// import type { Metadata } from 'next'
+// import { Analytics } from '@vercel/analytics/react';
+//
+// export const metadata: Metadata = {
+//   title: 'Roger Rojas - Software Architect & Full Stack Developer',
+//   description: 'Portfolio personal de Roger Rojas, Software Architect y Full Stack Developer especializado en crear experiencias digitales excepcionales.',
+// }
+//
+// export default function RootLayout({
+//                                      children,
+//                                    }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//       <html lang="es" suppressHydrationWarning>
+//       <body>
+//       <ThemeProvider>
+//         {children}
+//           <Analytics />
+//       </ThemeProvider>
+//       </body>
+//       </html>
+//   )
+// }
+
+
 // src/app/layout.tsx
 import { ThemeProvider } from '@/components/providers/ThemeProvider'
-import GoogleAnalytics from '@/components/providers/GoogleAnalytics'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script'
 
 export const metadata: Metadata = {
     title: 'Roger Rojas - Software Architect & Full Stack Developer',
@@ -17,15 +46,24 @@ export default function RootLayout({
 }) {
     return (
         <html lang="es" suppressHydrationWarning>
+        <head>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-8RTG6CG3GF"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8RTG6CG3GF');
+          `}
+            </Script>
+        </head>
         <body>
         <ThemeProvider>
             {children}
             <Analytics />
-            {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
-                <GoogleAnalytics
-                    GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}
-                />
-            )}
         </ThemeProvider>
         </body>
         </html>

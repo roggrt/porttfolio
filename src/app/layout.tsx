cf6352f3272cb8edcd5117341cb514c55fefbf56
roggrt <42749056+roggrt@users.noreply.github.com>
2024-10-28 08:27:28 -0500

    analytics by vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { ThemeProvider } from '@/components/providers/ThemeProvider'
 import './globals.css'
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Roger Rojas - Software Architect & Full Stack Developer',
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
       <ThemeProvider>
         {children}
+          <Analytics />
       </ThemeProvider>
       </body>
       </html>

761c5715c5b6151367d22f5382a1e370396fe521
roggrt <42749056+roggrt@users.noreply.github.com>
2024-11-06 20:11:25 -0500

    cambios de la robotica.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,68 @@
+// import { MetadataRoute } from 'next'
+
+// export default function robots(): MetadataRoute.Robots {
+//     return {
+//         rules: {
+//             userAgent: '*',
+//             allow: '/',
+//             disallow: '/api/', 
+//         },
+//         sitemap: 'https://torisoftt/sitemap.xml',
+//     }
+// }
+
+
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/api/', // Protege tus endpoints de API
-        },
-        sitemap: 'https://torisoftt/sitemap.xml',
+        rules: [
+            {
+                userAgent: '*',
+                allow: [
+                    '/',                    // Página principal que contiene todas las secciones
+                    '/blog/*',              // Blog y sus posts
+                    '/services/*',          // Páginas de servicios individuales
+                    '/privacy',             // Política de privacidad
+                    '/terms',               // Términos de servicio
+                ],
+                disallow: [
+                    '/api/*',               // API endpoints
+                ],
+            },
+
+            {
+                userAgent: 'Googlebot',
+                allow: '/',
+                crawlDelay: 1,
+            },
+            {
+                userAgent: 'Bingbot',
+                allow: '/',
+                crawlDelay: 1,
+            },
+            {
+                userAgent: 'Yandex',
+                allow: '/',
+                crawlDelay: 1,
+            },
+            {
+                userAgent: 'Baiduspider',
+                allow: '/',
+                crawlDelay: 1,
+            },
+
+
+            
+        ],
+        sitemap: 'https://torisoftt.com/sitemap.xml',
+        host: 'https://torisoftt.com'
     }
 }
+
+
+
+
+
+
+
